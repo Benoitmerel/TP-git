ffc6a7e0a6f3d334851818791d59d649de45dfd9
Benoit Merel <b.merel@iia-laval.fr>
2015-03-20 15:48:11 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,7 @@
-### application.bat - 201x - Merel ###
- echo "Copyright Benoit MEREL - 201x"
-echo "Build : 10:00"
-echo "version 1.1"
+### application.bat - 2015 - Merel ###
+ echo "Copyright Benoit MEREL - 2015"
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
